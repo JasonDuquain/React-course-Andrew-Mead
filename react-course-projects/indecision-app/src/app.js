8b3fee28e9d6fce8552aa43e9e5b19b9927630f8
jasonduquain <jduquain@gmail.com>
2018-06-07 22:16:14 -0400

    setting up babel complete - good start point

---
+++ react-course-projects/indecision-app/src/app.js
@@ -0,0 +1,11 @@
+
+console.log('app.js is running');
+
+// JSX - JS XML
+
+var template = <h1>Indecision App</h1>;
+var appRoot = document.getElementById('app');
+
+
+ReactDOM.render(template, appRoot);
+
